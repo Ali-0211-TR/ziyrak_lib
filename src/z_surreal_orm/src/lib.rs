729f87f46a184aeab2a187d84cebaab57f22de96
ali-0211-tr <ali-0211-tr@WorldWideTree.WorldWideTree>
2023-07-21 11:08:30 +0500

    Update doc for z_surreal_orm. Updated . DOC#3

--- src/z_surreal_orm/src/lib.rs
+++ src/z_surreal_orm/src/lib.rs
@@ -3,19 +3,16 @@
 //! По суте это всего лишь рассширение для клиента Surreal от Rust SDK `surrealdb`.
 //! Добились этого через создание трейтов и специально реализовав их для клиента `surrealdb::Surreal` и `surrealdb::method::Query`.
 //! 
-//! #Examples
-//! 
-//! 
-//! 
-//! 
 //! Реализованные методы SurrealQL:
-//! | -------- | ---------
-//! | `SELECT` |
-//! | `CREATE` |
-//! | `INSERT` |
-//! | `DELETE` |
-//! | `UPDATE` |
-//! ------------------------
+//! -------------------------------
+//! - `SELECT` -- Выборка элементов из записей
+//! - `CREATE` -- Создание записей
+//! - `INSERT` -- Вставка данных в записи
+//! - `DELETE` -- Удаление записей
+//! - `UPDATE` -- Обновление записей
+//! 
+//! 
+//! 
 
 
 pub(crate) mod core;
@@ -46,14 +43,73 @@ use surrealdb::{
 #[async_trait]
 pub trait ZrkSurrealOrm<C: Connection> {
     type QueryType<'a>: 'a where Self: 'a;
-    
-    /// Метод аналог `query`, но с маленьким отличием, в аргументах ожидается STATEMENT из нашего ORM
-    /// Возвращает то же что и `query`  
+    /// Метод аналогичен `query`, но с небольшим отличием: в аргументах ожидается `Statement` из нашего ORM.
+    /// Возвращает то же, что и `query`.
+    ///
+    /// # Arguments
+    ///
+    /// * `statement`: Объект типов которые реализовали `Statement` из нашего ORM, представляющий SQL-запрос.
+    ///
+    /// # Example
+    /// ```rust
+    /// // Including trait and statements
+    /// use z_surreal_orm::{
+    ///     ZrkSurrealOrm,
+    ///     create, select
+    /// };
+    ///
+    /// // Select the namespace/database to use
+    /// db.use_ns("namespace").use_db("database").await?;
+    ///
+    /// let tb_name = "person";
+    ///
+    /// // Run queries
+    /// let mut ress = db
+    ///     .zrk_orm_statement(create(tb_name))
+    ///     .zrk_orm_statement(select(vec![]).from(vec![tb_name]))
+    ///     .await?;
+    ///
+    /// // Get the first result from the first query
+    /// let created: Option<Person> = ress.take(0)?;
+    ///
+    /// // Get all of the results from the second query
+    /// let people: Vec<Person> = ress.take(1)?;
+    /// ```
     fn zrk_orm_statement<'a, S: Statement>(&'a self, statement: S) -> Self::QueryType<'a>;
 
-
-    /// Метод аналог `zrk_orm_statement`, но с отличием того что в принимает `SqlQuery` из нашего
-    /// ORM, это своего рода целый набор SQL запросов, возвращает оно `surrealdb::Response`
+    /// Метод аналогичен `zrk_orm_statement`, но принимает `SqlQuery` из нашего ORM, представляющий набор SQL-запросов.
+    /// Возвращает `surrealdb::Response`.
+    ///
+    /// # Arguments
+    ///
+    /// * `sql`: Объект `SqlQuery` из нашего ORM, представляющий целый набор SQL-запросов.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// // Including trait and statements
+    /// use z_surreal_orm::{
+    ///     ZrkSurrealOrmQuery,
+    ///     create, select,
+    ///     SqlQuery
+    /// };
+    /// 
+    /// // Select the namespace/database to use
+    /// db.use_ns("namespace").use_db("database").await?;
+    /// 
+    /// // Run queries
+    /// let mut ress = db.zrk_orm_sql_query(
+    ///     SqlQuery::new()
+    ///     .add_create(create("person"))
+    ///     .add_select(select(vec![]).from(vec!["person"]))
+    /// ).await?;
+    /// 
+    /// // Get the first result from the first query
+    /// let created: Option<Person> = ress.take(0)?;
+    /// 
+    /// // Get all of the results from the second query
+    /// let people: Vec<Person> = ress.take(1)?;
+    /// ```
     async fn zrk_orm_sql_query(&self, sql: SqlQuery) -> surrealdb::Result<Response>;
 }
 #[async_trait]
@@ -87,8 +143,38 @@ impl<C: Connection> ZrkSurrealOrm<C> for Surreal<C> {
     }
 }
 pub trait ZrkSurrealOrmQuery<'a, C: Connection> {
-    /// Метод аналог `query`, но с маленьким отличием, в аргументах ожидается STATEMENT из нашего ORM
-    /// Возвращает то же что и `query`  
+    /// Метод аналогичен `query`, но с небольшим отличием: в аргументах ожидается `Statement` из нашего ORM.
+    /// Возвращает то же, что и `query`.
+    ///
+    /// # Arguments
+    ///
+    /// * `statement`: Объект типов которые реализовали `Statement` из нашего ORM, представляющий SQL-запрос.
+    ///
+    /// # Example
+    /// ```rust
+    /// // Including trait and statements
+    /// use z_surreal_orm::{
+    ///     ZrkSurrealOrm,
+    ///     create, select
+    /// };
+    ///
+    /// // Select the namespace/database to use
+    /// db.use_ns("namespace").use_db("database").await?;
+    ///
+    /// let tb_name = "person";
+    ///
+    /// // Run queries
+    /// let mut ress = db
+    ///     .zrk_orm_statement(create(tb_name))
+    ///     .zrk_orm_statement(select(vec![]).from(vec![tb_name]))
+    ///     .await?;
+    ///
+    /// // Get the first result from the first query
+    /// let created: Option<Person> = ress.take(0)?;
+    ///
+    /// // Get all of the results from the second query
+    /// let people: Vec<Person> = ress.take(1)?;
+    /// ```
     fn zrk_orm_statement<S: Statement>(self, statement: S) -> method::Query<'a, C>;
 }
 
